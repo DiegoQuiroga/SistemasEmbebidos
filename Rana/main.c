ab2bba5c4e44e4a1fe17ac713840cede0cc0199a
Diego Quiroga <diego.quiroga@correo.usa.edu.co>
2020-08-07 22:23:54 -0500

    v2.0

--- Rana/main.c
+++ Rana/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    float x,y,d;
+    float saltos;
+
+    printf("ingrese x:");scanf("%f",&x);
+    printf("ingrese y:");scanf("%f",&y);
+    printf("ingrese d:");scanf("%f",&d);
+
+    if(x<=y&&x>0&&d>0){
+        saltos=ceil((y-x)/d);
+        printf("La rana debe dar minimo %.1f saltos\n\n",saltos);
+        int i;
+        printf("<-");
+        for(i=0;i<saltos;i++){
+        printf("-*-");
+}
+        printf("->\n\n");
+    }else
+        return 0;
     return 0;
 }
