92491a07209bdb6d626e79e9887cae3657e52503
Diego Quiroga <diego.quiroga@correo.usa.edu.co>
2020-08-11 09:08:18 -0500

    v1.0

--- Tres_num/main.c
+++ Tres_num/main.c
@@ -2,6 +2,46 @@
 
 int main()
 {
-    printf("Hello World!\n");
+
+    int numeros,cent,dece,unid,i,aux;
+    char mat_numero[20][20][20]={
+        {"  --  ",  "|   |",  "      ", "|   |", "  --  " },
+        {"      ",  "    |",  "      ", "    |", "      " },
+        {"  --  ",  "    |",  "  --  ", "|    ", "  --  " },
+        {"  --  ",  "    |",  "  --  ", "    |", "  --  " },
+        {"      ",  "|   |",  "  --  ", "    |", "      " },
+        {"  --  ",  "|    ",  "  --  ", "    |", "  --  " },
+        {"      ",  "|    ",  "  --  ", "|   |", "  --  " },
+        {"  --  ",  "    |",  "      ", "    |", "      " },
+        {"  --  ",  "|   |",  "  --  ", "|   |", "  --  " },
+        {"  --  ",  "|   |",  "  --  ", "    |", "      " },
+       };
+
+do{
+    printf("Ingresar 3 numeros a visualizar: ");
+    scanf("%d",&numeros);
+    aux = numeros;
+    cent = numeros%10;
+    numeros = numeros/10;
+    dece = numeros%10;
+    numeros = numeros/10;
+    unid = numeros%10;
+    numeros = numeros/10;
+
+    printf("El numero ingresado es %d %d %d\n",unid,dece,cent);
+
+    for (i=0;i<5;i++)
+    {
+        printf("%s\t", mat_numero[unid][i]);
+        printf("%s\t", mat_numero[dece][i]);
+        printf("%s\n", mat_numero[cent][i]);
+    }
+
+
+}while(aux>=0&&aux<=999);
+
+    printf("Error!!!\nNumero fuera del rango\n");
+
     return 0;
 }
+
